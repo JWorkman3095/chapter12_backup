3d60ca73b5ffac9d4e5e778af6f4d764530c4099
Jeff Workman <jworkhollr@aol.com>
2022-04-24 10:58:13 -0400

    back up after grad

---
+++ CustomerMaintenance/CustomerMaintenance/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerMaintenance {
+    public class Customer 
+    { // "public" makes the Method availble to other classes.
+        //creating the Constructor - same 
+        public Customer()
+        {
+        }
+
+        public Customer(string firstName, string lastName, string eMail) =>
+            (this.FirstName, this.LastName, this.Email) = (firstName, lastName, eMail);
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string GetDisplayText() => FirstName + " " + LastName + ", " + Email;
+        // Thi "GetDisplayText" Method returns a string that concatenates the code
+
+    }
+}
